b04c8be7e9af7dcdfefcd3ad9055ba89e54ff480
Jules Morelli <jmorelli@manas.tech>
2020-04-21 18:44:31 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from './App.module.scss';
+
+function App() {
+  return (
+    <div className={style.root}>
+      <header className={style.header}>
+        <h1>an opinionated ReactJS dev journey</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
